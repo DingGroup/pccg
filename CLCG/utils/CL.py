c303a474e4b7dab5314a57ad70ab25c6ecf5ac51
Xinqiang Ding <xqding@umich.edu>
2021-10-06 16:08:01 -0400

    add contrastive_learning_numpy

--- CLCG/utils/CL.py
+++ CLCG/utils/CL.py
@@ -73,5 +73,76 @@ def contrastive_learning(log_q_noise, log_q_data,
 
     return alphas, F
 
+def contrastive_learning_numpy(log_q_noise, log_q_data,
+                               basis_noise, basis_data):
+
+    """
+    Contrastive learning coefficients
+
+    Parameters
+    ----------
+    log_q_noise: 1-dimensional array
+        the logrithm of probability density for noise data under the noise distribution
+    log_q_data: 1-dimensional array
+        the logrithm of probability density for target data under the noise distribution
+    basis_noise: 2-dimensional array
+        the design matrix contraining basis values of noise data for compute the logrithm of probablity density for the target distribution
+    basis_data: 2-dimensional array
+        the design matrix contraining basis values of target data for compute the logrithm of probablity density for the target distribution
+
+    Returns
+    -------
+    alpha: 
     
+    """
     
+    assert(basis_noise.shape[-1] == basis_data.shape[-1])
+    assert(len(log_q_noise) == basis_noise.shape[0])
+    assert(len(log_q_data) == basis_data.shape[0])
+
+    basis_size = basis_noise.shape[-1]
+    alphas = np.zeros(basis_size)
+    F = np.zeros(1)
+    
+    x_init = np.concatenate([alphas, F])
+
+    log_q = np.concatenate([log_q_noise, log_q_data])
+    y = np.concatenate([np.zeros_like(log_q_noise),
+                        np.ones_like(log_q_data)])
+
+    basis = np.concatenate([basis_noise, basis_data])
+
+    num_samples_p = basis_data.shape[0]
+    num_samples_q = basis_noise.shape[0]
+
+    log_nu = np.log(num_samples_q / float(num_samples_p))
+    
+    def compute_loss_and_grad(x):
+        alphas = x[0:basis_size]
+        F = x[-1]
+
+        ## compute loss = -(y*h - np.log(1 + np.exp(h)))
+        h = -(np.matmul(basis, alphas) - F) - log_q - log_nu
+        loss = -(y*h - (np.maximum(h, 0) + np.log(1 + np.exp(-np.abs(h)))))
+        loss = np.mean(loss, 0)
+
+        ## compute gradients
+        p = 1. / (1 + np.exp(-np.abs(h)))
+        p[h < 0] = 1 - p[h < 0]
+
+        grad_alphas = np.matmul(basis.T, y - p) / y.shape[0]
+        grad_F = -np.mean(y - p, keepdims = True)
+        
+        grad = np.concatenate([grad_alphas, grad_F])
+
+        return loss, grad
+
+    loss, grad = compute_loss_and_grad(x_init)
+    x, f, d = optimize.fmin_l_bfgs_b(compute_loss_and_grad,
+                                     x_init,
+                                     iprint = 1)
+
+    alphas = x[0:basis_size]
+    F = x[-1]
+
+    return alphas, F
